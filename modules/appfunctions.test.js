cb4a0917d377e8fb1ca11ab9ecce3ca8050ec9c0
SamTush <samo4421@icloud.com>
2023-03-09 13:19:05 +0300

    Add test.js

--- modules/appfunctions.test.js
+++ modules/appfunctions.test.js
@@ -1,49 +1,50 @@
-import {
-  tasksArray,
-  saveToLocalStorage,
+import { JSDOM } from 'jsdom';
+import 'jest-localstorage-mock';
+
+const dom = new JSDOM(
+  '<!DOCTYPE html><html><body><ul class="todo-list"></ul></body></html>',
+);
+global.document = dom.window.document;
+global.window = dom.window;
+
+const {
   addTask,
   removeTask,
-  EditTask,
-  removeAllCompleted,
-} from './appfunctions.js';
+  displayTasks,
+  tasksArray,
+} = require('./appfunctions.js');
 
-describe('Task Manager', () => {
+describe('addTask', () => {
   beforeEach(() => {
     localStorage.clear();
     tasksArray.length = 0;
+    displayTasks();
   });
 
-  test('Adding a task', () => {
-    const taskInput = { value: 'Task 1' };
-    addTask(taskInput);
-    expect(tasksArray.length).toBe(1);
-    expect(tasksArray[0].description).toBe('Task 1');
+  test('adds a task to the list', () => {
+    const input = document.createElement('input');
+    input.value = 'New task';
+    addTask(input);
+    expect(tasksArray).toHaveLength(1);
+    expect(document.querySelectorAll('.task-li')).toHaveLength(1);
+  });
+});
+
+describe('removeTask', () => {
+  beforeEach(() => {
+    localStorage.clear();
+    tasksArray.length = 0;
+    tasksArray.push({
+      description: 'Task 1',
+      completed: false,
+      index: 0,
+    });
+    displayTasks();
   });
 
-  test('Removing a task', () => {
-    tasksArray.push({ description: 'Task 1', completed: false, index: 0 });
-    saveToLocalStorage();
+  test('removes a task from the list', () => {
     removeTask(tasksArray[0]);
-    expect(tasksArray.length).toBe(0);
-  });
-
-  test('Editing a task', () => {
-    tasksArray.push({ description: 'Task 1', completed: false, index: 0 });
-    saveToLocalStorage();
-    const taskInput = { value: 'Task 1 edited', closest: () => ({ id: 'task-0' }) };
-    EditTask(taskInput);
-    expect(tasksArray[0].description).toBe('Task 1 edited');
-  });
-
-  test('Removing all completed tasks', () => {
-    tasksArray.push(
-      { description: 'Task 1', completed: true, index: 0 },
-      { description: 'Task 2', completed: false, index: 1 },
-      { description: 'Task 3', completed: true, index: 2 },
-    );
-    saveToLocalStorage();
-    removeAllCompleted();
-    expect(tasksArray.length).toBe(1);
-    expect(tasksArray[0].description).toBe('Task 2');
+    expect(tasksArray).toHaveLength(0);
+    expect(document.querySelectorAll('.task-li')).toHaveLength(0);
   });
 });
